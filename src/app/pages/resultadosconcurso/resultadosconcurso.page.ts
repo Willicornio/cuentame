69c20184e961d4f4f2ebf282b34e56b29ab5ce0d
STLERV <annafrias.9@gmail.com>
2020-09-26 20:26:17 +0200

    pagina resultado concurso

--- src/app/pages/resultadosconcurso/resultadosconcurso.page.ts
+++ src/app/pages/resultadosconcurso/resultadosconcurso.page.ts
@@ -19,6 +19,12 @@ export class ResultadosconcursoPage implements OnInit {
   listainscripcipnes: [];
   listalibros: Libro[];
   muestralista: Libro[];
+  primerpuesto;
+  segundopuesto;
+  tercerpuesto;
+  libro1;
+  libro2;
+  libro3;
 
   constructor(private router: Router, private peticionesAPI: PeticionesapiService, public alertController: AlertController) { }
 
@@ -43,7 +49,9 @@ export class ResultadosconcursoPage implements OnInit {
         this.concurso.forEach(cosa => {
 
           this.listainscripcipnes = cosa.listaLibrosParticipantes;
-
+          this.primerpuesto = cosa.primerpuesto;
+          this.segundopuesto = cosa.segundopuesto;
+          this.tercerpuesto = cosa.tercerpuesto;
 
         })
 
@@ -59,6 +67,10 @@ export class ResultadosconcursoPage implements OnInit {
   obtenerlibros() {
     var i = 0;
     this.listalibros = [];
+    this.libro1 = new Libro();
+    this.libro2 = new Libro();
+    this.libro3 =  new Libro();
+
     this.listainscripcipnes.forEach(cosa => {
 
       this.peticionesAPI.dameunlibro(cosa)
@@ -67,6 +79,19 @@ export class ResultadosconcursoPage implements OnInit {
           this.listalibros.push(res);
 
           i = i + 1;
+          if( res.titulo == this.primerpuesto){
+            this.libro1 = res;
+
+          }
+          if( res.titulo == this.segundopuesto){
+            this.libro2 = res;
+
+          }
+          if( res.titulo == this.tercerpuesto){
+            this.libro3 = res;
+
+          }
+
           if (i == this.listainscripcipnes.length) {
             this.ordenarlibros();
 
@@ -97,21 +122,12 @@ export class ResultadosconcursoPage implements OnInit {
     this.muestralista = this.listalibros;
     this.muestralista.reverse();
     console.log(this.muestralista);
-    this.hacertoptres();
+
   }
 
-  hacertoptres() {
-    this.toptres = [];
-    var i = 0;
-    this.muestralista.forEach(cosa => {
-    if (i < 3) {
-      this.toptres.push(cosa)
-      i = i + 1;
 
-    }
-  });
-    console.log(this.toptres);
-  }
+
+  
 
 
 }
